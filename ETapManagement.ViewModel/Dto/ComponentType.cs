bb6f6552baaf61a0ae01aeeb43b74fa799047181
amalnath <cap.poobalan@gmail.com>
2020-12-06 15:22:02 +0800

    update component type model validation

--- ETapManagement.ViewModel/Dto/ComponentType.cs
+++ ETapManagement.ViewModel/Dto/ComponentType.cs
@@ -7,8 +7,20 @@ namespace ETapManagement.ViewModel.Dto
 	public class ComponentTypeDetails
 	{
 		public int Id { get; set; }
+		
+		[Required]
+        [DataType (DataType.Text)]
+        [StringLength (200)]
+        [Display (Name = "Component Name")]
 		public string Name { get; set; }
+		[Required]
+        [DataType (DataType.Text)]
+        [StringLength (500)]
+        [Display (Name = "Component Description")]
 		public string Description { get; set; }
+		
+		[Required]
+        [Display (Name = "Status")]
 		public bool isActive { get; set; }
 	}
 
