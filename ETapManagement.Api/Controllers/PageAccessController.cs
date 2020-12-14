e87d10818c6d15d05ee43239b1d4b6196d04a1f7
amalnath <cap.poobalan@gmail.com>
2020-12-14 11:55:08 +0800

    remove auth

--- ETapManagement.Api/Controllers/PageAccessController.cs
+++ ETapManagement.Api/Controllers/PageAccessController.cs
@@ -16,7 +16,7 @@ using Serilog;
 namespace ETapManagement.Api.Controllers {
 
     [EnableCors ("AllowAll")]
-    [Authorize]
+    //[Authorize]
     [ApiController]
     //[ValidateAntiForgeryToken]
 
