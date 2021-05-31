e7ea4c7cad37c11db697da3f76756c0c1ae5626d
amalnath <cap.poobalan@gmail.com>
2021-06-01 01:43:41 +0800

    strategic bisiness database schema

---
+++ ETapManagement.Domain/Models/StrategicBusiness.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ETapManagement.Domain.Models
+{
+    public partial class StrategicBusiness
+    {
+        public int Id { get; set; }
+        public int BuId { get; set; }
+        public string Name { get; set; }
+        public bool IsDelete { get; set; }
+        public bool? IsActive { get; set; }
+        public int? CreatedBy { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public int? UpdatedBy { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+        public virtual BusinessUnit Bu { get; set; }
+    }
+}
